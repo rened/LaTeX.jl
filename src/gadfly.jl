4e88cf6992f3e02ab346f948294a438c27d2cd8f
Jonathan Bieler <jonathan.bieler@alumni.epfl.ch>
2020-10-02 16:02:39 +0200

    added support for stacked plots

--- src/gadfly.jl
+++ src/gadfly.jl
@@ -1,13 +1,14 @@
 import .Gadfly
 import Cairo, Fontconfig
 
-Image(height, width, a::Gadfly.Plot) =
+Image(height, width, a::Union{Gadfly.Plot, Gadfly.Compose.Context}) =
     Image(height, width, ImageFileData(height, width, a))
 
-function ImageFileData(height, width, a::Gadfly.Plot)
+function ImageFileData(height, width, a::Union{Gadfly.Plot, Gadfly.Compose.Context})
     filename = tempname()*".pdf"
     Gadfly.draw(Gadfly.PDF(filename, width*Gadfly.cm, height*Gadfly.cm), a)
     r = read(filename)
     rm(filename)
     ImageFileData(r, :pdf)
 end
+
